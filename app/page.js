ce0e5d687fbf5e783bc281fc6b7a50355ab515ea
ddangmul <j.ddangmul@gmail.com>
2025-01-31 16:17:54 +0900

    use() & Suspense  & 에러바운더리 컴포넌트

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
+import fs from "node:fs/promises";
+
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import { Suspense } from "react";
+import { resolve } from "node:path";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      // resolve(users);
+      reject(new Error("Error!"));
+    }, 2000)
+  );
+
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
